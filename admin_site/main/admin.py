78b1c87593c66723738a060f978e66a951a6f10b
ayaz <ayazikmur@gmail.com>
2025-06-20 19:43:43 +0500

    наработки по добавлению новостей

--- admin_site/main/admin.py
+++ admin_site/main/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Employee, News
+from django.utils.html import format_html
 
 admin.site.register(Employee)
-admin.site.register(News)
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'display_image')
+    readonly_fields = ('display_image',)
+
+    def display_image(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="150" />', obj.image.url)
+        return "—"
+
+    display_image.short_description = 'Превью'
--- admin_site/main/models.py
+++ admin_site/main/models.py
@@ -10,9 +10,14 @@ class Employee(models.Model):
         return self.name
 
 class News(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField('Заголовок', max_length=200)
+    image = models.ImageField('Главное изображение', upload_to='news/', blank=True, null=True)
+    content = models.TextField('Текст новости')  # Можно использовать HTML-теги
+    date = models.DateTimeField('Дата публикации', auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
