a802facbd02cfdadd930d863a85021f6cbe3f36f
ayaz <ayazikmur@gmail.com>
2025-06-21 14:32:04 +0500

    test

--- admin_site/admin_site/wsgi.py
+++ admin_site/admin_site/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+path = '/home/ayazmur/karmaskali'  # путь к вашему проекту
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
