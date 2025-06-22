ec53b01b9d58f8995c576680d6a26612b390fc5e
ayaz <ayazikmur@gmail.com>
2025-06-22 12:47:51 +0500

    Revert "пробуем в автообновление"
    This reverts commit 9e6cec0f59b9964b6be45f23bb26c938ad9fd6ed.

--- admin_site/admin_site/urls.py
+++ admin_site/admin_site/urls.py
@@ -1,29 +1,12 @@
-import subprocess
-
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.decorators.csrf import csrf_exempt
-
-
-@csrf_exempt
-def github_webhook(request):
-    if request.method == 'POST':
-        try:
-            # Запускаем скрипт обновления
-            subprocess.run(["/home/вашusername/auto-update.sh"], check=True)
-            return HttpResponse("Сайт успешно обновлен!", status=200)
-        except Exception as e:
-            return HttpResponse(f"Ошибка: {e}", status=500)
-    return HttpResponse("Только POST-запросы разрешены", status=400)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('webhook/', github_webhook),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
