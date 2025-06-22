01d2bea4f776de3a783f96393f63fd837e1a7d4b
ayaz <ayazikmur@gmail.com>
2025-06-22 15:41:06 +0500

    наработки по новостям

--- admin_site/main/views.py
+++ admin_site/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Employee, News, Gallery
 
 
@@ -13,11 +13,13 @@ def employees(request):
     return render(request, 'employees.html', {'employees': employees_list})
 
 def news(request):
-    news_list = News.objects.all().order_by('-date')
+    news_list = News.objects.all().select_related('author')
     return render(request, 'news.html', {'news': news_list})
+
 def news_detail(request, news_id):
-    news_item = News.objects.get(id=news_id)
+    news_item = get_object_or_404(News.objects.select_related('author'), id=news_id)
     return render(request, 'news_detail.html', {'news': news_item})
+
 def contact(request):
     if request.method == 'POST':
         # Обработка формы (можно добавить сохранение в БД или отправку на почту)
