d5ec39768782e0e1e985c9bb66f1ab96ca201f4a
ayaz <ayazikmur@gmail.com>
2025-06-28 16:42:58 +0500

    добавил поиск по новостям и пагинацию в новостях

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Employee, News, Gallery
 
@@ -17,8 +18,22 @@ def employees(request):
 
 
 def news(request):
-    news_list = News.objects.all().select_related('author')
-    return render(request, 'news/news.html', {'news': news_list})
+    news_list = News.objects.all().order_by('-date')
+
+    # Добавляем поиск
+    search_query = request.GET.get('q')
+    if search_query:
+        news_list = news_list.filter(title__icontains=search_query)
+
+    # Добавляем пагинацию (10 новостей на страницу)
+    paginator = Paginator(news_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'news/news.html', {
+        'page_obj': page_obj,
+        'search_query': search_query,
+    })
 
 
 def news_detail(request, news_id):
