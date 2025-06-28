122486106bb4a160ad5770afe23b7017ffdb5c4c
ayaz <ayazikmur@gmail.com>
2025-06-28 16:32:34 +0500

    добавил гитигнор и скрыл ключи

--- admin_site/settings.py
+++ admin_site/settings.py
@@ -12,18 +12,20 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$)71ynh(r=rv4ny+i10w=$&mf%=8o5(i8@k6#i03ri9x0qo_+8'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", 'False') == 'True'
 
 ALLOWED_HOSTS = ['ayazmur.pythonanywhere.com',
                  '127.0.0.1',
